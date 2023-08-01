f3ed8d2fdbb57fb4ebccdf6494a890b320026417
LatikDesu <evga.abrosimov@gmail.com>
2023-08-01 18:03:06 +0300

    tests

--- app/routers/pereval.py
+++ app/routers/pereval.py
@@ -12,8 +12,26 @@ router = APIRouter(tags=["SubmitData"])
 
 @router.post("/SubmitData",
              summary="Отправка данных о перевале для обработки",
-             description="Отправка данных о перевале для обработки",
-             response_model=SubmitDataResponse)
+             response_model=SubmitDataResponse,
+             description=
+             """
+Objective:
+The 'submit_data' function is used to receive data about a mountain pass from a user, process it, and store it in a database. The function updates user information if the user already exists in the database, creates a new user if they do not exist, and creates a new entry for the mountain pass in the database.
+
+Inputs:
+- request: an instance of the 'PerevalPost' class, containing information about the mountain pass and the user who submitted the data.
+
+Flow:
+1. The function checks if the user who submitted the data already exists in the database.
+2. If the user exists, their information is updated. If not, a new user is created.
+3. The function creates a new entry in the 'coords' table of the database, containing the latitude, longitude, and height of the mountain pass.
+4. The function creates a new entry in the 'pereval_add' table of the database, containing information about the mountain pass, the user who submitted the data, and the coordinates of the mountain pass.
+5. The function creates new entries in the 'pereval_images' table of the database for each image submitted with the data.
+6. The function returns a 'SubmitDataResponse' object with a status code, a message, and the ID of the new entry in the 'pereval_add' table.
+
+Outputs:
+- SubmitDataResponse object: an instance of the 'SubmitDataResponse' class, containing a status code, a message, and the ID of the new entry in the 'pereval_add' table.
+             """, )
 async def submit_data(request: PerevalPost):
     query = users_table.select().where(users_table.c.email == request.user.email)
     check_user = await database.fetch_one(query)
---
+++ app/tests/SubmitData.py
@@ -0,0 +1,207 @@
+# Dependencies:
+# pip install pytest-mock
+import pytest
+from fastapi import HTTPException
+
+from app.db_connection import database
+from app.models.schemas import PerevalPost, UserModel, CoordsModel, LevelModel, ImagesModel
+from app.routers.pereval import submit_data
+
+
+class TestSubmitData:
+    #  Tests that valid input data is submitted successfully
+    @pytest.mark.asyncio
+    async def test_valid_input_data(self, mocker):
+        request = PerevalPost(
+            beauty_title='Test Beauty Title',
+            title='Test Title',
+            other_titles='Test Other Titles',
+            connect='Test Connect',
+            user=UserModel(
+                email='test@test.com',
+                fam='Test Fam',
+                name='Test Name',
+                otc='Test Otc',
+                phone='Test Phone'
+            ),
+            coords=CoordsModel(
+                latitude=1.0,
+                longitude=2.0,
+                height=3
+            ),
+            level=LevelModel(
+                winter='Test Winter',
+                summer='Test Summer',
+                autumn='Test Autumn',
+                spring='Test Spring'
+            ),
+            images=[ImagesModel(data='Test Data', name='Test Name')]
+        )
+        mocker.patch('app.routers.submit_data.database', database)
+        mocker.patch('app.routers.submit_data.images_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.pereval_add_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.coords_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.select', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.update', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.insert', return_value=1)
+        result = await submit_data(request)
+        assert result.status == 200
+        assert result.message == 'Отправлено успешно'
+        assert result.id == 1
+
+    #  Tests that user already in the database is updated successfully
+    @pytest.mark.asyncio
+    async def test_existing_user(self, mocker):
+        request = PerevalPost(
+            beauty_title='Test Beauty Title',
+            title='Test Title',
+            other_titles='Test Other Titles',
+            connect='Test Connect',
+            user=UserModel(
+                email='test@test.com',
+                fam='Test Fam',
+                name='Test Name',
+                otc='Test Otc',
+                phone='Test Phone'
+            ),
+            coords=CoordsModel(
+                latitude=1.0,
+                longitude=2.0,
+                height=3
+            ),
+            level=LevelModel(
+                winter='Test Winter',
+                summer='Test Summer',
+                autumn='Test Autumn',
+                spring='Test Spring'
+            ),
+            images=[ImagesModel(data='Test Data', name='Test Name')]
+        )
+        mocker.patch('app.routers.submit_data.database', database)
+        mocker.patch('app.routers.submit_data.images_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.pereval_add_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.coords_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.select', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.update', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.insert', return_value=1)
+        result = await submit_data(request)
+        assert result.status == 200
+        assert result.message == 'Отправлено успешно'
+        assert result.id == 1
+
+    #  Tests that new user is added to the database successfully
+    @pytest.mark.asyncio
+    async def test_new_user(self, mocker):
+        request = PerevalPost(
+            beauty_title='Test Beauty Title',
+            title='Test Title',
+            other_titles='Test Other Titles',
+            connect='Test Connect',
+            user=UserModel(
+                email='test@test.com',
+                fam='Test Fam',
+                name='Test Name',
+                otc='Test Otc',
+                phone='Test Phone'
+            ),
+            coords=CoordsModel(
+                latitude=1.0,
+                longitude=2.0,
+                height=3
+            ),
+            level=LevelModel(
+                winter='Test Winter',
+                summer='Test Summer',
+                autumn='Test Autumn',
+                spring='Test Spring'
+            ),
+            images=[ImagesModel(data='Test Data', name='Test Name')]
+        )
+        mocker.patch('app.routers.submit_data.database', database)
+        mocker.patch('app.routers.submit_data.images_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.pereval_add_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.coords_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.select', return_value=None)
+        mocker.patch('app.routers.submit_data.users_table.update', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.insert', return_value=1)
+        result = await submit_data(request)
+        assert result.status == 200
+        assert result.message == 'Отправлено успешно'
+        assert result.id == 1
+
+    #  Tests that invalid input data is not submitted
+    @pytest.mark.asyncio
+    async def test_invalid_input_data(self, mocker):
+        request = PerevalPost(
+            beauty_title='Test Beauty Title',
+            title='Test Title',
+            other_titles='Test Other Titles',
+            connect='Test Connect',
+            user=UserModel(
+                email='test@test.com',
+                fam='Test Fam',
+                name='Test Name',
+                otc='Test Otc',
+                phone='Test Phone'
+            ),
+            coords=CoordsModel(
+                latitude=1.0,
+                longitude=2.0,
+                height=3
+            ),
+            level=LevelModel(
+                winter='Test Winter',
+                summer='Test Summer',
+                autumn='Test Autumn',
+                spring='Test Spring'
+            ),
+            images=[ImagesModel(data='Test Data', name='Test Name')]
+        )
+        mocker.patch('app.routers.submit_data.database', database)
+        mocker.patch('app.routers.submit_data.images_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.pereval_add_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.coords_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.select', return_value=None)
+        mocker.patch('app.routers.submit_data.users_table.update', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.insert', return_value=1)
+        request.user.email = None
+        with pytest.raises(HTTPException):
+            await submit_data(request)
+
+    #  Tests that user email is required
+    @pytest.mark.asyncio
+    async def test_missing_user_email(self, mocker):
+        request = PerevalPost(
+            beauty_title='Test Beauty Title',
+            title='Test Title',
+            other_titles='Test Other Titles',
+            connect='Test Connect',
+            user=UserModel(
+                email=None,
+                fam='Test Fam',
+                name='Test Name',
+                otc='Test Otc',
+                phone='Test Phone'
+            ),
+            coords=CoordsModel(
+                latitude=1.0,
+                longitude=2.0,
+                height=3
+            ),
+            level=LevelModel(
+                winter='Test Winter',
+                summer='Test Summer',
+                autumn='Test Autumn',
+                spring='Test Spring'
+            ),
+            images=[ImagesModel(data='Test Data', name='Test Name')]
+        )
+        mocker.patch('app.routers.submit_data.database', database)
+        mocker.patch('app.routers.submit_data.images_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.pereval_add_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.coords_table.insert', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.select', return_value=None)
+        mocker.patch('app.routers.submit_data.users_table.update', return_value=1)
+        mocker.patch('app.routers.submit_data.users_table.insert', return_value=1)
+        with pytest.raises(HTTPException):
+            await submit_data(request)
